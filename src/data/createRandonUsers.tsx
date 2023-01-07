8fb1ff5c087d03aa94d008ef40c86c0f24887e7f
Julian <julianmartinez_93@hotmail.com>
2023-01-07 13:28:12 -0300

    create exel with data of  firebase

--- src/data/createRandonUsers.tsx
+++ src/data/createRandonUsers.tsx
@@ -2,15 +2,17 @@ import { faker } from '@faker-js/faker'
 import { formInitialValues } from '@/models/formTypes'
 import { db } from '@/config/firebaseConfig'
 import { collection, addDoc, getDocs } from 'firebase/firestore'
+import { arrayCountries } from '@/utils'
 
 const createRandomUsers = (count: number): formInitialValues[] => {
+  const countryesArray = arrayCountries.map((country) => country.value)
   const users: formInitialValues[] = []
   for (let i = 0; i < count; i++) {
     users.push({
       full_name: faker.name.fullName(),
       email: faker.internet.email(),
       birth_date: faker.date.past(18).toISOString().split('T')[0],
-      country_of_origin: faker.address.country(),
+      country_of_origin: faker.helpers.arrayElement(countryesArray),
       terms_and_conditions: 'true',
     })
   }
---
+++ src/pages/results/components/asDataExel/AsDataExel.tsx
@@ -0,0 +1,27 @@
+import { utils, writeFile } from 'xlsx'
+import fileSaver from 'file-saver'
+import { Button } from 'react-bootstrap'
+import Swal from 'sweetalert2'
+
+interface Props {
+  data: any
+}
+
+export default function AsDataExel ({ data }: Props) {
+  console.log(data)
+  const handleClick = () => {
+    if (!data) return Swal.fire('Error', 'No hay datos para exportar', 'error')
+    const ws = utils.json_to_sheet(data)
+    const wb = utils.book_new()
+    utils.book_append_sheet(wb, ws, 'Sheet1')
+    const file = writeFile(wb, 'test.xlsx')
+    const blob = new Blob([file], { type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' })
+    fileSaver.saveAs(blob, 'test.xlsx')
+  }
+
+  return (
+    <>
+      <Button type='button' variant='info' onClick={handleClick}>Descarga Exel</Button>
+    </>
+  )
+}
