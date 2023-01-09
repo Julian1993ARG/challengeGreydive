10bceefc6df9265c4c2522fcbcda86601e94348f
Julian <julianmartinez_93@hotmail.com>
2023-01-09 03:08:48 -0300

    add GA4

--- src/app.tsx
+++ src/app.tsx
@@ -3,12 +3,18 @@ import './app.css'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import '@/config/firebaseConfig'
 import { createRandonUsers } from '@/data'
+import ReactGA from 'react-ga4'
 
 const HomeLazy = lazy(() => import('./pages/home/Home'))
 const ResultsLazy = lazy(() => import('./pages/results/Results'))
 
+const googleAnalyticsId = import.meta.env.VITE_GOOGLE_ANALYTICS_ID
+
 export function App () {
-  useEffect(() => { createRandonUsers(15) }, [])
+  useEffect(() => {
+    createRandonUsers(15)
+    ReactGA.initialize(googleAnalyticsId)
+  }, [])
   return (
     <div className='App'>
       <BrowserRouter>
